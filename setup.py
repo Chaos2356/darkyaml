c2e04c86907aeac0a275e400f20d48fded9bc9d8
xllwhoami <86256058+xllwhoami@users.noreply.github.com>
2022-05-10 09:48:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
    name='darkyaml',
-   version='0.0.2',
+   version='0.0.3',
    description='DarkYml is simple yaml db',
    long_description=long_description,
    long_description_content_type='text/markdown',
